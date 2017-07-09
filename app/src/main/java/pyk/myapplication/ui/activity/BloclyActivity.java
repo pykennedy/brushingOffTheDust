e8e4b925987ed695d8af24d735cd5c91178dabe4
Peter Kennedy <pykennedy@gmail.com>
2017-07-08 22:17:23 -0700

    first commit

---
+++ app/src/main/java/pyk/myapplication/ui/activity/BloclyActivity.java
@@ -0,0 +1,14 @@
+package pyk.myapplication.ui.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import pyk.myapplication.R;
+
+public class BloclyActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_blocly);
+    }
+}
